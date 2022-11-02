627e58590916b33b624dcb0d1082643f13d13794
asmy8h <sinhaasmit99@gmail.com>
2022-11-02 22:05:36 +0530

    made a inputfield function

---
+++ lib/widgets/inputfield.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+
+class InputField extends StatelessWidget {
+  final keyboardType;
+  final String hintxt;
+  const InputField(
+      {super.key, required this.keyboardType, required this.hintxt});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300,
+      child: TextFormField(
+        keyboardType: keyboardType,
+        style: const TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+        decoration: InputDecoration(
+            hintText: hintxt,
+            hintStyle:
+                const TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(3),
+                borderSide: BorderSide.none),
+            filled: true,
+            fillColor: Colors.white54),
+        validator: (value) {
+          if (value!.isEmpty) {
+            return "$hintxt  cannot be empty";
+          }
+          return null;
+        },
+      ),
+    );
+  }
+}
